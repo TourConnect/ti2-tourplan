57d75c7718b42c0281d60ccc740980384fc13e0a
Salvador Aceves <salvador@aceves.mx>
2022-10-19 22:15:38 +0400

    add a template for the token

--- index.js
+++ index.js
@@ -31,6 +31,25 @@ class Plugin {
     Object.entries(params).forEach(([attr, value]) => {
       this[attr] = value;
     });
+    this.tokenTemplate = () => ({
+      endpoint: {
+        type: 'text',
+        regExp: /^(?!mailto:)(?:(?:http|https|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:(\/|\?|#)[^\s]*)?$/i,
+        description: 'The uri for the tourplan adapter',
+      },
+      username: {
+        type: 'text',
+        regExp: /.+/,
+        description: 'The tourplan provided username',
+        default: 'en',
+      },
+      password: {
+        type: 'text',
+        regExp: /.+/,
+        description: 'The tourplan provided password',
+        default: 'en',
+      },
+    });
   }
 
   async validateToken({
